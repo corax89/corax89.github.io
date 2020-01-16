75953f420aee4233a3171f4691a49bf6d63da7cd
corax89 <corax8989@yandex.ru>
2020-01-16 16:17:05 +0300

    support opera 12.18

--- esp8266Game/ui.js
+++ esp8266Game/ui.js
@@ -75,6 +75,42 @@ sourceArea.onkeydown = sourceArea.onkeyup = sourceArea.onkeypress = sourceArea.o
 		});
 	}
 })();
+// http://paulirish.com/2011/requestanimationframe-for-smart-animating/
+// http://my.opera.com/emoller/blog/2011/12/20/requestanimationframe-for-smart-er-animating
+
+// requestAnimationFrame polyfill by Erik Möller. fixes from Paul Irish and Tino Zijdel
+
+// MIT license
+
+(function() {
+    var lastTime = 0;
+    var vendors = ['ms', 'moz', 'webkit', 'o'];
+    for(var x = 0; x < vendors.length && !window.requestAnimationFrame; ++x) {
+        window.requestAnimationFrame = window[vendors[x]+'RequestAnimationFrame'];
+        window.cancelAnimationFrame = window[vendors[x]+'CancelAnimationFrame'] 
+                                   || window[vendors[x]+'CancelRequestAnimationFrame'];
+    }
+ 
+    if (!window.requestAnimationFrame)
+        window.requestAnimationFrame = function(callback, element) {
+            var currTime = new Date().getTime();
+            var timeToCall = Math.max(0, 16 - (currTime - lastTime));
+            var id = window.setTimeout(function() { callback(currTime + timeToCall); }, 
+              timeToCall);
+            lastTime = currTime + timeToCall;
+            return id;
+        };
+ 
+    if (!window.cancelAnimationFrame)
+        window.cancelAnimationFrame = function(id) {
+            clearTimeout(id);
+        };
+}());
+
+if (typeof document.getElementById("inputImgHighlite").scrollTo !== 'function') {
+	isHighliteColor = false;
+	document.getElementById("highliteColorCheckbox").style.display = 'none';
+}
 
 window.addEventListener("unload", function() {
   localStorage.setItem('save_source_code', sourceArea.value);
@@ -558,8 +594,10 @@ function inputOnKey(e) {
 }
 
 function handleScroll() {
-	var h = document.getElementById("inputImgHighlite");
-	h.scrollTo(sourceArea.scrollLeft, sourceArea.scrollTop);
+	if(isHighliteColor){
+		var h = document.getElementById("inputImgHighlite");
+		h.scrollTo(sourceArea.scrollLeft, sourceArea.scrollTop);
+	}
 }
 
 function pixelColorHighlight(){
